ce690b1de7e0b7eb35b9680d238bac609b9be32a
seonnn <ssni0105@gmail.com>
2024-01-06 23:53:36 +0900

    vite config 빌드 관련 설정 추가

--- packages/practice-ui/vite.config.ts
+++ packages/practice-ui/vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      // 진입점(여러 진입점은 객체 또는 배열로 지정할 수 있음)
+      entry: resolve(__dirname, 'index.ts'),
+      formats: ["es", "umd"]
+    },
+    rollupOptions: {
+      // 라이브러리에 포함하지 않을 디펜던시
+      external: ['react'],
+      output: {
+        // 라이브러리 외부에 존재하는 디펜던시를 위해 UMD(Universal Module Definition) 번들링 시 사용될 전역 변수
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  }
 })
